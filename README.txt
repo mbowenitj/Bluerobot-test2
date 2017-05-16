c171d87baedfb22cfdf3b8a062696caa8c3acba5
tshepo <mbowenitshepo@gmail.com>
2017-05-16 13:27:25 -0700

    animated progress bar completed

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/anon/pen/BRxgjz.
+
+ Here is an animated progress bar using CSS transitions and jQuery animation.
---
+++ app/index.route.js
@@ -0,0 +1,44 @@
+(function() {
+  'use strict';
+
+  angular.module('blueRobot')
+    .config(routerConfig);
+
+  /** @ngInject */
+  function routerConfig($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('index', {
+        url: '/',
+        templateUrl: 'app/index.html',
+        controller: 'CartCtrl'
+      })
+
+      .state('projects', {
+        url: '/projects',
+        // templateUrl: 'app/projects/projects.html',
+        controller: 'ProjectsCtrl'
+      });;
+
+    $urlRouterProvider.otherwise('/');
+  }
+
+  run.$inject = ['$rootScope', '$location', '$cookies', '$http'];
+    function run($rootScope, $location, $cookies, $http) {
+
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookies.getObject('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata;
+        }
+
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in and trying to access a restricted page
+            var restrictedPage = $.inArray($location.path(), ['/']) === -1;
+            var loggedIn = $rootScope.globals.currentUser;
+            if (restrictedPage && !loggedIn) {
+                $location.path('/');
+            }
+        });
+    }
+
+})();
